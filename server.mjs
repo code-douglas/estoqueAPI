911a167b1b3e1dd833925b8fc4eaf13b09e45b4b
Douglas <douglasd3v.contato@gmail.com>
2025-08-25 14:15:41 +0000

    Separando responsabilidade do banco de dados para o db.mjs

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import app from './src/app.mjs';
+import connectDB from './src/config/db.mjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Falha ao iniciar o servidor:', err);
+  });
