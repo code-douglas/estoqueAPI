58a55f07dc0c6b34e74177c1e1d0ae80ec8fdee1
Douglas <douglasd3v.contato@gmail.com>
2025-08-25 20:32:08 +0000

    Adicionando rotas e fixando bug onde não havia retorno dos produtos cadastrados.

--- src/routes/productsRoutes.mjs
+++ src/routes/productsRoutes.mjs
@@ -2,16 +2,22 @@ import { Router } from 'express';
 import {
   createProduct,
   getProductById,
-  getAllProducts
+  getAllProducts,
+  updateProduct,
+  deleteProduct
 } from '../controllers/productController.mjs';
 
 const router = Router();
 
 // Criar novo produto
 router.post('/registrar', createProduct);
-// Pegar produto pelos parametros
-router.get('/:id', getProductById);
 // Listar produtos
 router.get('/catalogo', getAllProducts);
+// Pegar produto pelos parametros
+router.get('/:id', getProductById);
+// Atualizar produto
+router.put('/atualizar/:id', updateProduct);
+// Deletar produtos
+router.delete('/excluir/:id', deleteProduct);
 
 export default router;
