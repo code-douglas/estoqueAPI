3067707b29b2134b63987b5cf15539a7acfff486
Douglas <douglasd3v.contato@gmail.com>
2025-08-25 18:17:46 +0000

    Adicionando rota /produtos ao app.mjs

--- src/app.mjs
+++ src/app.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import productsRouter from './routes/productsRoutes.mjs';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/produtos', productsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API de Estoque online!' });
