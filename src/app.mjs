c983cdcb0b9530fa36eb14aa668246c8e52e7129
Douglas <douglasd3v.contato@gmail.com>
2025-08-25 20:48:06 +0000

    Adicionando rota exata para transações no sistema

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import productsRouter from './routes/productsRoutes.mjs';
+import transactionsRouter from './routes/transactionsRoutes.mjs';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/produtos', productsRouter);
+app.use('/transacoes', transactionsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API de Estoque online!' });
