1ca32f4ec387225d0ac5f5a7225e957f89e2730a
Douglas <douglasd3v.contato@gmail.com>
2025-09-01 17:43:56 -0300

    Removing Firebase, add auth by jwt/bycript

--- src/routes/productsRoutes.mjs
+++ src/routes/productsRoutes.mjs
@@ -6,19 +6,18 @@ import {
   updateProduct,
   deleteProduct
 } from '../controllers/productController.mjs';
-import { authenticateToken } from '../middlewares/authMiddleware.mjs';
 
 const router = Router();
 
 // Criar novo produto
-router.post('/registrar', authenticateToken, createProduct);
+router.post('/registrar', createProduct);
 // Listar produtos
 router.get('/catalogo', getAllProducts);
 // Pegar produto pelos parametros
 router.get('/:id', getProductById);
 // Atualizar produto
-router.put('/atualizar/:id', authenticateToken, updateProduct);
+router.put('/atualizar/:id', updateProduct);
 // Deletar produtos
-router.delete('/excluir/:id', authenticateToken, deleteProduct);
+router.delete('/excluir/:id', deleteProduct);
 
 export default router;
